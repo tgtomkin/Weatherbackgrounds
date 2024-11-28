a4f085e670e8a80cc8b28b24398c2265cff29801
tgtomkin <tomkins.tomtom.thomas@gmail.com>
2024-11-28 16:47:57 -0500

    Added Error Handling
    Added error handling and error messages for opening settings file, user selecting no folder during first startup, when download fails and if writing the images fail.
    
    Added get_pic_filepath which requests the user to pick the filepath and asks them to confirm their choice if no path is selected
    
    added updated code to .exe and to .zip

--- Weatherbackgrounds.py
+++ Weatherbackgrounds.py
@@ -5,25 +5,50 @@ import requests
 import schedule
 import os
 from tkinter import filedialog
-
 # pics_url = "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/CONUS/GEOCOLOR/latest.jpg"
 
+# create timestamp text for messages
 def build_timestamp():
     time_val = time.strftime('%Y-%m-%d %H:%M', time.localtime(time.time()))
     time_string = "[" + time_val + "] - "
     return time_string
 
+# print out settings values of pas
+def print_settings(settings):
+    print("URL: " + settings[0])
+    print("Folder: " + settings[1])
+    print("Time between downloads: " + settings[2])
+
+# return generic settings list, print out values
+def generic_settings():
+    settings_list = ["https://cdn.star.nesdis.noaa.gov/GOES16/ABI/CONUS/GEOCOLOR/latest.jpg", "", "20"]
+    print("Settings set to generic values:")
+    print_settings(settings_list)
+    return settings_list
+
+# pull settings.txt file in same folder as .exe to code
+# reverts to generic values if the opening of the file fails
 def open_settings_file():
-    # pull settings file in relative path to code
-    with open('settings.txt', mode = 'r', encoding= 'utf-8-sig') as settings_file:
-        lines = settings_file.readlines()
     settings_list = []
+    try:
+        with open('settings.txt', mode = 'r', encoding= 'utf-8-sig') as settings_file:
+            lines = settings_file.readlines()
+    except IOError as e:
+        print(build_timestamp() + "IO Error when reading settings file Error{0}: {1}".format(e.errno, e.strerror))
+        settings_list = generic_settings()
+        return settings_list
+    except:
+        print(build_timestamp() + "Reading settings file failed")
+        settings_list = generic_settings()
+        return settings_list
+
     for line in lines:  # each setting is at the second position in each line, pull each value to be used later
         line = line.split(",")
         line = [i.strip() for i in line]
         settings_list.append(line[1])
     return settings_list
 
+# attempt to download picture from given URL
 def download_pics(picture_url):
     try:
         img_data = requests.get(picture_url).content
@@ -32,33 +57,77 @@ def download_pics(picture_url):
         img_data = 0 # sets to 0 to be ignored before the store is attempted
     return  img_data
 
+# takes photo data and overwrites current pics at file_path
+# creates two to have the photos properly update when picture updates for windows slideshow option
 def store_pics(picture_data, file_path):
-    # takes photo data and overwrites current pics at file_path
-    # creates two to have the photos properly update when picture updates for windows slideshow option
     file1 = os.path.join(file_path,'latest1.jpg')
     file2 = os.path.join(file_path,'latest2.jpg')
-    with open(file1, 'wb') as handle:
-        handle.write(picture_data)
+    any_errors = 0
+    try:
+        # attempt to write first picture to folder
+        with open(file1, 'wb') as handle:
+            handle.write(picture_data)
+    except IOError as e:
+        # write error codes to output
+        print("Error occurred when storing latest1.jpg please ensure the file is not open")
+        print("I/O error({0}): {1}".format(e.errno, e.strerror))
+        any_errors = 1
+    except:
+        # else, write generic error
+        print("Unexpected Error occurred when storing latest1.jpg please ensure the file is not open")
+        any_errors = 1
+    # if the first write worked, attempt to write second picture with same errors on failure
+    else:
+        try:
+            with open(file2,'wb') as handle2:
+                handle2.write(picture_data)
+        except IOError as e:
+            print("Error occurred when storing latest2.jpg please ensure the file is not open")
+            print("I/O error({0}): {1}".format(e.errno, e.strerror))
+            any_errors = 1
+        except:
+            print("Unexpected Error occurred when storing latest2.jpg please ensure the file is not open")
+            any_errors = 1
+    return any_errors
 
-    with open(file2,'wb') as handle2:
-        handle2.write(picture_data)
 
 def download_and_store_pic(file_location, pic_url):
     print(build_timestamp() + "starting download")
     pic_data = download_pics(pic_url)
     if pic_data != 0:
-        print(build_timestamp() + "picture exists")
-        store_pics(pic_data,file_location)
-        print(build_timestamp() + "hey it worked")
+        print(build_timestamp() + "download worked")
+        pic_storing_errors = store_pics(pic_data,file_location)
+        if pic_storing_errors == 0:
+            print(build_timestamp() + "hey it worked")
+
+
+def get_pic_filepath():
+    pic_filepath = ""
+    while pic_filepath == "":
+        pic_filepath = filedialog.askdirectory()
+        if pic_filepath == "":
+            print("No folder selected, pictures will be placed in the same folder as Weatherbackgrounds.exe")
+            print("Are you sure? Please type y/n to confirm")
+            user_input = input()
+            if user_input in ["y", "n"]:
+                if user_input == "n":
+                    continue
+                if user_input == "y":
+                    break
+            else:
+                print("Please answer y/n")
+    return pic_filepath
+
 
 def main():
     # have user select filepath on first run
     previous_settings = open_settings_file()
     picture_url = previous_settings[0] # set URL to settings.txt URL
     minutes_per_run = previous_settings[2] # set minutes_per_run to settings.txt minutes
+
     # check if filepath setting is empty, and request user to select filepath and update settings file accordingly
     if previous_settings[1] == '':
-        pic_filepath = filedialog.askdirectory()
+        pic_filepath = get_pic_filepath()
         with open('settings.txt', mode= 'w', encoding= 'utf-8-sig') as settings_file:
             settings_file.write("url, " + picture_url + "\noutput folder, "+
                                 pic_filepath + "\nminutes, " + minutes_per_run)
@@ -66,11 +135,6 @@ def main():
     else:
         pic_filepath = previous_settings[1]
 
-    """
-    picture_url = pics_url
-    minutes_per_run = 2
-    pic_filepath = "H:/Pictures/gnds"
-    """
     # create schedule job to run with information from settings file and immediately run on startup
     schedule.every(int(minutes_per_run)).minutes.do(
         download_and_store_pic, file_location=pic_filepath, pic_url = picture_url)
